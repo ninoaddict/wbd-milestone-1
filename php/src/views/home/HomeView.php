8c576c97a48a7dcec2b0752362d208d22b1ff325
ninoaddict <adrilbless37@gmail.com>
2024-10-15 23:37:38 +0700

    feat: add navbar (incomplete)

--- php/src/views/home/HomeView.php
+++ php/src/views/home/HomeView.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/public/css/preflight.css">
   <link rel="stylesheet" href="/public/css/globals.css">
+  <link rel="stylesheet" href="/public/css/navbar.css">
 
   <title>Home</title>
 </head>
+
 <body>
-  <h1>Home</h1>
+  <?php include dirname(__DIR__) . '/components/Navbar.php' ?>
+  <h1>Selamat Datang</h1>
 </body>
+
 </html>
