2aad55a7ff942d8d6c62dd5764436414a65aab84
fabianradenta <13522105@mahasiswa.itb.ac.id>
2024-10-21 16:23:44 +0700

    fix: correct SQL query in company profile model

--- php/src/models/CompanyProfileModel.php
+++ php/src/models/CompanyProfileModel.php
@@ -11,7 +11,7 @@ class CompanyProfileModel {
     }
 
     public function queryCompanyProfile(int $id){
-        $sql = "SELECT user_id, nama, lokasi, about FROM company_detail cd JOIN users u ON cd.user_id=u.user_id WHERE cd.user_id=:id";
+        $sql = "SELECT cd.user_id, u.nama, cd.lokasi, cd.about FROM company_detail cd JOIN users u ON cd.user_id=u.user_id WHERE cd.user_id=:id";
         $statement = $this->db->prepare($sql);
         $this->db->bind($statement,':id', $id);
         $this->db->execute($statement);
