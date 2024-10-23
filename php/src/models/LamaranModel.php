ebe145c5159ec25291bc6075dce473ee473e5186
fabianradenta <13522105@mahasiswa.itb.ac.id>
2024-10-23 22:20:46 +0700

    refactor: move function from HistoryModel to LamaranModel

--- php/src/models/LamaranModel.php
+++ php/src/models/LamaranModel.php
@@ -142,4 +142,38 @@ class LamaranModel {
     $res = $this->db->fetch($stmt);
     return $res['nama'];
   }
+
+  public function getAllLamaranHistory(int $user_id){
+    $sql = "SELECT low.lowongan_id, u.nama, low.posisi, TO_CHAR(lam.created_at, 'YYYY-MM-DD') as created_time, lam.status
+            FROM lamaran lam JOIN lowongan low 
+                ON lam.lowongan_id=low.lowongan_id 
+                JOIN users u 
+                    ON u.user_id=low.company_id 
+            WHERE lam.user_id=:user_id 
+            ORDER BY lam.created_at 
+            DESC";
+            
+    $statement = $this->db->prepare($sql);
+    $this->db->bind($statement, ":user_id", $user_id);
+    $this->db->execute($statement);
+    return $statement->fetchAll();
+  }
+
+  public function getSelectedLamaranHistory(int $user_id, string $status){
+    $sql = "SELECT low.lowongan_id, u.nama, low.posisi, TO_CHAR(lam.created_at, 'YYYY-MM-DD') as created_time, lam.status
+            FROM lamaran lam JOIN lowongan low 
+                ON lam.lowongan_id=low.lowongan_id 
+                JOIN users u 
+                    ON u.user_id=low.company_id 
+            WHERE lam.user_id=:user_id 
+            AND lam.status=:status
+            ORDER BY lam.created_at 
+            DESC";
+            
+    $statement = $this->db->prepare($sql);
+    $this->db->bind($statement, ":user_id", $user_id);
+    $this->db->bind($statement, ":status", $status);
+    $this->db->execute($statement);
+    return $statement->fetchAll();
+  }
 }
