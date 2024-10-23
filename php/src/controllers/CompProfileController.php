d9b07f01e51cbdff9628862b07822f6dc235ffb5
fabianradenta <13522105@mahasiswa.itb.ac.id>
2024-10-23 23:40:12 +0700

    chore: add missing file to previous commit

--- php/src/controllers/CompProfileController.php
+++ php/src/controllers/CompProfileController.php
@@ -5,20 +5,21 @@ use app\core\Application;
 use app\core\Controller;
 use app\core\Request;
 use app\models\CompanyProfileModel;
+use app\models\UserModel;
 use Exception;
 
 
 class CompProfileController extends Controller {   
     private int $params = 0;
-    private CompanyProfileModel $companyProfileModel;
+    private UserModel $userModel;
 
     public function __construct() {
-        $this->companyProfileModel = new CompanyProfileModel();
+        $this->userModel = new UserModel();
     }
 
     public function profilePage(Request $request){
         $user_id = $_SESSION["user_id"];
-        $companyProfile = $this->companyProfileModel->getCompanyProfile($user_id);
+        $companyProfile = $this->userModel->getCompanyProfile($user_id);
         $path = __DIR__ . '/../views/profile/ProfileView.php';
         $this->render($path, ['companyProfile' => $companyProfile]);
     }
@@ -27,7 +28,7 @@ class CompProfileController extends Controller {
         $user_id = $_SESSION['user_id'];
         $body = $request->getBody();
 
-        if ($this->companyProfileModel->updateCompanyProfile($user_id, $body['nama'], $body['lokasi'], $body['about'])){
+        if ($this->userModel->updateCompanyProfile($user_id, $body['nama'], $body['lokasi'], $body['about'])){
             $_SESSION['nama'] = $body['nama'];
             header('Location: /profile');
             exit();
